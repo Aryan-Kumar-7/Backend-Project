a87efeb38d3805305e791220e8085cd92340469e
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-01-27 17:33:29 +0530

    controller routes models

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-
-import mongoose, { connect } from "mongoose"
-import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({
     path: './env'
